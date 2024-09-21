9528aa2fa6f24a431644937aad95d29230d88077
kangsudal <kangsudal@gmail.com>
2024-09-21 09:00:24 +0900

    App에 Firebase Project 연결하기

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:eat_go/screen/home_screen/home_screen.dart';
 import 'package:eat_go/screen/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
