bc0ef4eabcec8778709882ad58899ee47ff1eedb
kangsudal <kangsudal@gmail.com>
2024-10-19 02:57:40 +0900

    사용자 설정 값들을 변경 후 저장 안하고 페이지를 나갔을때 설정 상태 복원

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -32,15 +32,6 @@ class _SettingScreenState extends ConsumerState<SettingScreen> {
 
   @override
   void dispose() {
-    final currentUser = ref.read(settingViewModelProvider).asData?.value;
-    // 로컬 상태와 Firestore에서 불러온 원래 상태를 비교
-    if (originalUser != null &&
-        currentUser != null &&
-        currentUser != originalUser) {
-      final settingViewModel = ref.read(settingViewModelProvider.notifier);
-      // 만약 저장되지 않은 변경사항이 있다면, 원래 상태로 되돌림
-      settingViewModel.resetToOriginal(originalUser);
-    }
     super.dispose();
   }
 
@@ -49,139 +40,186 @@ class _SettingScreenState extends ConsumerState<SettingScreen> {
     final settingViewState = ref.watch(settingViewModelProvider);
     final settingViewModel = ref.read(settingViewModelProvider.notifier);
 
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            title: const Text('SETTING'),
-          ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  settingViewState.when(
-                    loading: () => const CircularProgressIndicator(),
-                    error: (error, stackTrace) {
-                      debugPrint('SettingScreen 오류 발생 - 회원 설정 화면: $error');
-                      // 에러 발생 시 SnackBar 표시
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('오류가 발생했습니다.')),
-                      );
-                      return const Text('오류 발생');
-                    },
-                    data: (EatGoUser? user) {
-                      if (user == null) {
-                        debugPrint(
-                            'SettingScreen 오류 발생 - 회원 설정 화면: EatGoUser 리턴값이 null입니다.');
-                        return const Text('로그인된 사용자가 없습니다.');
-                      }
-                      return Column(
+    return PopScope(
+      //뒤로 가기 동작이 발생했을 때 호출
+      onPopInvokedWithResult: (bool didPop, dynamic result) {
+        if (didPop) {
+          // 페이지가 닫히기 전에 상태를 복원(저장안하고 떠났을때)
+          final currentUser = ref.read(settingViewModelProvider).asData?.value;
+          // 로컬 상태와 Firestore에서 불러온 원래 상태를 비교
+          if (originalUser != null &&
+              currentUser != null &&
+              currentUser != originalUser) {
+            final settingViewModel =
+                ref.read(settingViewModelProvider.notifier);
+            // 만약 저장되지 않은 변경사항이 있다면, 수정 전 원래 상태로 되돌림
+            settingViewModel.resetToOriginal(originalUser);
+          }
+        }
+      },
+      child: Stack(
+        children: [
+          Scaffold(
+            appBar: AppBar(
+              title: const Text('SETTING'),
+            ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    settingViewState.when(
+                      loading: () => const CircularProgressIndicator(),
+                      error: (error, stackTrace) {
+                        debugPrint('SettingScreen 오류 발생 - 회원 설정 화면: $error');
+                        // 에러 발생 시 SnackBar 표시
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('오류가 발생했습니다.')),
+                        );
+                        return const Text('오류 발생');
+                      },
+                      data: (EatGoUser? user) {
+                        if (user == null) {
+                          debugPrint(
+                              'SettingScreen 오류 발생 - 회원 설정 화면: EatGoUser 리턴값이 null입니다.');
+                          return const Text('로그인된 사용자가 없습니다.');
+                        }
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text('안녕하세요! ${user.displayName}님.'),
+                            ),
+                            ListTile(
+                              leading: Text('Email:'),
+                              title: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                child: Text(user.email),
+                              ),
+                            ),
+                            ListTile(
+                              title: const Text('푸쉬 알림설정'),
+                              trailing: CupertinoSwitch(
+                                value: user.pushNotificationEnabled,
+                                onChanged: (isEnabled) {
+                                  // 로컬 상태 업데이트 (Firestore에는 아직 저장하지 않음)
+                                  settingViewModel
+                                      .togglePushNotification(isEnabled);
+                                },
+                                activeColor: pointColor,
+                                trackColor: Colors.black,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        // 저장 버튼을 눌렀을 때 Firestore에 저장 로직 호출
+                        final result = await settingViewModel.saveSettings();
+                        if (result) {
+                          if (context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('설정이 저장되었습니다.')),
+                            );
+                          }
+                        } else {
+                          if (context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('설정 저장이 실패 되었습니다.')),
+                            );
+                          }
+                        }
+                      },
+                      child: const Text('저장'),
+                    ),
+                    const Divider(
+                      thickness: 1,
+                    ),
+                    ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          ListTile(
-                            title: Text('안녕하세요! ${user.displayName}님.'),
+                          TextButton(
+                            child: const Text('로그아웃'),
+                            onPressed: () async {
+                              // 페이지가 닫히기 전에 상태를 복원(저장안하고 떠났을때)
+                              final currentUser = ref
+                                  .read(settingViewModelProvider)
+                                  .asData
+                                  ?.value;
+                              // 로컬 상태와 Firestore에서 불러온 원래 상태를 비교
+                              if (originalUser != null &&
+                                  currentUser != null &&
+                                  currentUser != originalUser) {
+                                final settingViewModel =
+                                    ref.read(settingViewModelProvider.notifier);
+                                // 만약 저장되지 않은 변경사항이 있다면, 수정 전 원래 상태로 되돌림
+                                settingViewModel.resetToOriginal(originalUser);
+                              }
+                              try {
+                                await settingViewModel.signOut();
+                              } catch (e) {
+                                settingViewModel.resetToOriginal(originalUser);
+                              }
+                            },
                           ),
-                          ListTile(
-                            leading: Text('Email:'),
-                            title: FittedBox(
-                              fit: BoxFit.scaleDown,
-                              child: Text(user.email),
-                            ),
-                          ),
-                          ListTile(
-                            title: const Text('푸쉬 알림설정'),
-                            trailing: CupertinoSwitch(
-                              value: user.pushNotificationEnabled,
-                              onChanged: (isEnabled) {
-                                // 로컬 상태 업데이트 (Firestore에는 아직 저장하지 않음)
-                                settingViewModel
-                                    .togglePushNotification(isEnabled);
-                              },
-                              activeColor: pointColor,
-                              trackColor: Colors.black,
-                            ),
+                          TextButton(
+                            child: const Text('탈퇴하기'),
+                            onPressed: () async {
+                              try{
+                                handleWithdrawal(context);
+                              }catch(e){
+                                debugPrint('SettingScreen - $e');
+                              }
+                            },
                           ),
                         ],
-                      );
-                    },
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      // 저장 버튼을 눌렀을 때 Firestore에 저장 로직 호출
-                      final result = await settingViewModel.saveSettings();
-                      if (result) {
-                        if (context.mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('설정이 저장되었습니다.')),
-                          );
-                        }
-                      } else {
-                        if (context.mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('설정 저장이 실패 되었습니다.')),
-                          );
-                        }
-                      }
-                    },
-                    child: const Text('저장'),
-                  ),
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  ListTile(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          child: const Text('로그아웃'),
-                          onPressed: () async {
-                            await settingViewModel.signOut();
-                          },
-                        ),
-                        TextButton(
-                          child: const Text('탈퇴하기'),
-                          onPressed: () async {
-                            handleWithdrawal(context);
-                          },
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
+            // 로딩 상태 처리 (Stack을 사용해 전체 화면에 CircularProgressIndicator 추가)
           ),
-          // 로딩 상태 처리 (Stack을 사용해 전체 화면에 CircularProgressIndicator 추가)
-        ),
-        if (settingViewState is AsyncLoading)
-          Container(
-            color: Colors.black.withOpacity(0.5), // 반투명 배경
-            child: const Center(
-              child: CircularProgressIndicator(), // 중앙에 로딩 인디케이터
+          if (settingViewState is AsyncLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5), // 반투명 배경
+              child: const Center(
+                child: CircularProgressIndicator(), // 중앙에 로딩 인디케이터
+              ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 
   void handleWithdrawal(BuildContext context) async {
     final settingViewModel = ref.read(settingViewModelProvider.notifier);
+    // 페이지가 닫히기 전에 상태를 복원(저장안하고 떠났을때)
+    final currentUser = ref.read(settingViewModelProvider).asData?.value;
+    // 로컬 상태와 Firestore에서 불러온 원래 상태를 비교
+    if (originalUser != null &&
+        currentUser != null &&
+        currentUser != originalUser) {
+      // 만약 저장되지 않은 변경사항이 있다면, 수정 전 원래 상태로 되돌림
+      settingViewModel.resetToOriginal(originalUser);
+      try {
+        // 계정 및 데이터 삭제
+        final result = await settingViewModel.deleteUserAccountAndData();
 
-    // 계정 및 데이터 삭제
-    final result = await settingViewModel.deleteUserAccountAndData();
-
-    if (result) {
-      // 사용자 로드 성공 시 화면 이동
-      if (context.mounted) {
-        context.go('/success_withdrawal');
-      }
-    } else if (result == false) {
-      debugPrint('Screen 오류 발생 - 회원 설정 화면: EatGoUser 리턴값이 null입니다.');
-      if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('오류가 발생했습니다.')),
-        );
+        if (result == false) {
+          debugPrint('Screen 오류 발생 - 회원 설정 화면: EatGoUser 리턴값이 null입니다.');
+          if (context.mounted) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('오류가 발생했습니다.')),
+            );
+          }
+        }
+      } catch (e) {
+        settingViewModel.resetToOriginal(originalUser);
       }
     }
   }
