6e076cc6b4bc68b758d5a27fee06e5f4e5dccf95
kangsudal <kangsudal@gmail.com>
2024-10-19 11:07:42 +0900

    getUwer()코드 위로 올림

--- lib/viewmodels/setting_viewmodel.dart
+++ lib/viewmodels/setting_viewmodel.dart
@@ -19,6 +19,34 @@ class SettingViewModel extends AsyncNotifier<EatGoUser?> {
     return await getUser();
   }
 
+  Future<EatGoUser?> getUser() async {
+    state = const AsyncValue.loading();
+    try {
+      String? currentUserUid = _authRepository.getCurrentUserUid();
+      if (currentUserUid != null) {
+        Map<String, dynamic>? userMap =
+        await _userRepository.getUser(currentUserUid);
+        // Firestore에서 사용자 데이터를 가져오지 못한 경우
+        if (userMap == null) {
+          state = AsyncValue.error('사용자 데이터를 가져올 수 없습니다.', StackTrace.current);
+          debugPrint('SettingViewModel 오류 발생 - userMap이 null로 반환되었습니다.');
+          return null;
+        }
+        EatGoUser user = EatGoUser.fromJson(userMap);
+        state = AsyncValue.data(user);
+        return user;
+      } else {
+        state = AsyncValue.error('사용자 UID를 가져올 수 없습니다.', StackTrace.current);
+        debugPrint('SettingViewModel 오류 발생 - getUser()값이 null로 반환');
+        return null;
+      }
+    } catch (e, stackTrace) {
+      state = AsyncValue.error(e, stackTrace);
+      debugPrint('SettingViewModel 오류 발생 - $e');
+    }
+    return null;
+  }
+
   // 사용자 계정 삭제 및 데이터 삭제
   Future<bool> deleteUserAccountAndData() async {
     state = const AsyncValue.loading();
@@ -52,34 +80,6 @@ class SettingViewModel extends AsyncNotifier<EatGoUser?> {
       state = AsyncValue.error(e, stackTrace);
       debugPrint('로그아웃 중 오류 발생: $e');
     }
-  }
-
-  Future<EatGoUser?> getUser() async {
-    state = const AsyncValue.loading();
-    try {
-      String? currentUserUid = _authRepository.getCurrentUserUid();
-      if (currentUserUid != null) {
-        Map<String, dynamic>? userMap =
-            await _userRepository.getUser(currentUserUid);
-        // Firestore에서 사용자 데이터를 가져오지 못한 경우
-        if (userMap == null) {
-          state = AsyncValue.error('사용자 데이터를 가져올 수 없습니다.', StackTrace.current);
-          debugPrint('SettingViewModel 오류 발생 - userMap이 null로 반환되었습니다.');
-          return null;
-        }
-        EatGoUser user = EatGoUser.fromJson(userMap);
-        state = AsyncValue.data(user);
-        return user;
-      } else {
-        state = AsyncValue.error('사용자 UID를 가져올 수 없습니다.', StackTrace.current);
-        debugPrint('SettingViewModel 오류 발생 - getUser()값이 null로 반환');
-        return null;
-      }
-    } catch (e, stackTrace) {
-      state = AsyncValue.error(e, stackTrace);
-      debugPrint('SettingViewModel 오류 발생 - $e');
-    }
-    return null;
   }
 
   // 상태를 변경하되 Firestore에 즉시 저장하지 않음
