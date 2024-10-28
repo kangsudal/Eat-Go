0b7e2058af87c44b1b5b93a3b8398cd0199ee328
kangsudal <kangsudal@gmail.com>
2024-10-29 02:01:04 +0900

    Podfile 버전 14로 바꿔서 구글맵 띄우는거까지 함

--- lib/screen/restaurant_screen/restaurant_screen.dart
+++ lib/screen/restaurant_screen/restaurant_screen.dart
@@ -2,8 +2,9 @@ import 'package:eat_go/screen/restaurant_screen/restaurant_screen_back_button.da
 import 'package:eat_go/screen/restaurant_screen/keyword_suggestion_card.dart';
 import 'package:eat_go/screen/restaurant_screen/scrollable_cards.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class RestaurantScreen extends StatelessWidget {
+class RestaurantScreen extends StatefulWidget {
   final String recipeId;
   const RestaurantScreen({
     super.key,
@@ -11,16 +12,36 @@ class RestaurantScreen extends StatelessWidget {
   });
 
   @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
     int itemCount = 10;
     return Scaffold(
-      backgroundColor: Colors.greenAccent,
+      // backgroundColor: Colors.greenAccent,
 
       extendBodyBehindAppBar: true,
       body: SafeArea(
         bottom: false,
         child: Stack(
           children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
             KeywordSuggestionCard(),
             RestaurantScreenBackButton(),
             ScrollableCards(itemCount: itemCount),
