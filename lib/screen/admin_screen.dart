b078c81f1db9f70d90ac4bf38b844ecf3ff4f03a
kangsudal <kangsudal@gmail.com>
2024-11-01 15:52:21 +0900

    recipe컬렉션의 recipeId 필드삭제, 모든 createdBy 내 계정으로 변경 쿼리 추가

--- lib/screen/admin_screen.dart
+++ lib/screen/admin_screen.dart
@@ -14,7 +14,7 @@ class AdminScreen extends StatelessWidget {
             ElevatedButton(
               onPressed: () async {
                 // 버튼 클릭 시 일회성 작업 실행
-                await updateAllRecipes();
+                // await updateRecipesCollection();
 
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
@@ -49,5 +49,30 @@ class AdminScreen extends StatelessWidget {
     // 일괄 업데이트 실행
     await batch.commit();
     print("모든 레시피 문서가 업데이트되었습니다.");
+  }
+
+  Future<void> updateRecipesCollection() async {
+    final firestore = FirebaseFirestore.instance;
+    final batch = firestore.batch();
+
+    try {
+      // recipes 컬렉션의 모든 문서 가져오기
+      final querySnapshot = await firestore.collection('recipes').get();
+
+      for (var doc in querySnapshot.docs) {
+        // batch에 업데이트 작업 추가: recipeId 필드를 삭제하고, createdBy 값을 업데이트
+        batch.update(doc.reference, {
+          'recipeId': FieldValue.delete(),  // recipeId 필드 삭제
+          'createdBy': 'DY1monVBKWcN2xVu17L91JqOgsh2',  // createdBy 값 업데이트
+        });
+      }
+
+      // batch 실행
+      await batch.commit();
+      print("recipes 컬렉션의 모든 문서가 성공적으로 업데이트되었습니다.");
+
+    } catch (e) {
+      print("업데이트 중 오류 발생: $e");
+    }
   }
 }
